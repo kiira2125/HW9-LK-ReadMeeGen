00213901b07e0873641ceb9c0efc62b2930a25a3
LK <lunakiira0911@gmail.com>
2022-05-21 19:01:40 -0700

    updates

---
+++ Develop/index.js
@@ -0,0 +1,15 @@
+const inq = require('inquirer');;
+const chalk = require('chalk');
+const fs = require('fs');
+const question = [
+
+]
+
+function createReadme() {
+    fs.writeFileSync ('./README.md','hello tesing')
+}
+
+
+
+createReadme();
+
